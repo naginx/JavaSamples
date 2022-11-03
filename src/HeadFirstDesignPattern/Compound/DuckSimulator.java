cc983ca4ea2ebf959f27da6860b358ab92c14322
naginx <miura.hnut@gmail.com>
2022-11-04 08:52:04 +0900

    Compoundパターン Factory編

--- src/HeadFirstDesignPattern/Compound/DuckSimulator.java
+++ src/HeadFirstDesignPattern/Compound/DuckSimulator.java
@@ -4,14 +4,15 @@ public class DuckSimulator {
 
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
-        simulator.simulate();
+        AbstractDuckFactory duckFactory = new CountingDuckFactory();
+        simulator.simulate(duckFactory);
     }
 
-    void simulate() {
-        Quackable mallardDuck = new QuackCounter(new MallardDuck());
-        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-        Quackable duckCall = new QuackCounter(new DuckCall());
-        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+    void simulate(AbstractDuckFactory duckFactory) {
+        Quackable mallardDuck = duckFactory.createMallardDuck();
+        Quackable redheadDuck = duckFactory.createRedheadDuck();
+        Quackable duckCall = duckFactory.createDuckCall();
+        Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseAdapter = new GooseAdapter(new Goose());
 
         System.out.println("\nカモシミュレータ");
